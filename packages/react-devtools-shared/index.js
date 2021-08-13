72b6b5f4aef55acd30f889a91805a24df3bf198d
Narin Luangrath <narin.luangrath@gmail.com>
2021-08-13 16:19:23 -0700

    add index.js file to react-devtools-shared

---
+++ packages/react-devtools-shared/index.js
@@ -0,0 +1,3 @@
+import { InspectedElementContext, InspectedElementContextController } from './src/devtools/views/Components/InspectedElementContext';
+
+export { InspectedElementContext, InspectedElementContextController };
